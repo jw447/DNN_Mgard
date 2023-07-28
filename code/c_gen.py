d1671a7f70d286b95c72378596239f18064513ba
Jinzhen Wang <jw447@njit.edu>
2023-07-28 13:17:15 -0400

    update

--- code/c_gen.py
+++ code/c_gen.py
@@ -7,23 +7,26 @@ import adios2 as ad
 from tqdm import tqdm
 
 def main():
-    f = open("c0_gen.txt", "a")
-    for v in tqdm(np.arange(65536)):
-        line = "{:1.3} 5.54 5.54 \n".format(np.random.randint(1, 554 + 1)/100 )
-        f.write(line)
-    f.close()
+    #f = open("c0_gen.txt", "a")
+    #for v in tqdm(np.arange(65536)):
+    #    #line = "{:1.3} 5.54 5.54 \n".format(np.random.randint(1, 554 + 1)/100 )
+    #    line = "{:1.3} 2.77 2.77 \n".format(np.random.randint(1, 554 + 1)/100 )
+    #    f.write(line)
+    #f.close()
 
-    f = open("c1_gen.txt", "a")
-    for v in tqdm(np.arange(65536)):
-        line = "5.54 {:1.3} 5.54 \n".format(np.random.randint(1, 554 + 1)/100 )
-        f.write(line)
-    f.close()
+    #f = open("c1_gen.txt", "a")
+    #for v in tqdm(np.arange(65536)):
+    #    #line = "5.54 {:1.3} 5.54 \n".format(np.random.randint(1, 554 + 1)/100 )
+    #    line = "2.77 {:1.3} 2.77 \n".format(np.random.randint(1, 554 + 1)/100 )
+    #    f.write(line)
+    #f.close()
 
-    f = open("c2_gen.txt", "a")
-    for v in tqdm(np.arange(65536)):
-        line = "5.54 5.54 {:1.3} \n".format(np.random.randint(1, 554 + 1)/100 )
-        f.write(line)
-    f.close()
+    #f = open("c2_gen.txt", "a")
+    #for v in tqdm(np.arange(65536)):
+    #    #line = "5.54 5.54 {:1.3} \n".format(np.random.randint(1, 554 + 1)/100 )
+    #    line = "2.77 2.77 {:1.3} \n".format(np.random.randint(1, 554 + 1)/100 )
+    #    f.write(line)
+    #f.close()
     
     f = open("c_gen.txt", "a")
     for v in tqdm(np.arange(65536)):
